35daad8a8dc4e5a63f51750099f2129c11cb97e7
Kane Valentine <kane@cute.im>
2018-02-20 09:11:13 +0000

    init connection to postgres server and read config from env

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,32 @@
+#[macro_use]
+extern crate serde_derive;
+extern crate serde;
+extern crate postgres;
+extern crate envy;
+
+use postgres::{Connection, TlsMode};
+
+#[derive(Deserialize, Debug)]
+struct postgres_config {
+  postgres_host: String,
+  postgres_port: u16,
+  postgres_db: String,
+  postgres_user: String,
+  postgres_password: String,
+}
+
 fn main() {
-    println!("Hello, world!");
+    let pg_config = match envy::from_env::<postgres_config>() {
+        Ok(postgres_config) => postgres_config,
+        Err(error) => panic!("Error reading Postgres configuration: {:#?}", error)
+    };
+
+    let connection_string = format!("postgres://{}:{}@{}:{}/{}", pg_config.postgres_user,
+        pg_config.postgres_password, pg_config.postgres_host, pg_config.postgres_port, 
+        pg_config.postgres_db);
+
+    let conn = match Connection::connect(connection_string, TlsMode::None) {
+        Ok(c) => c,
+        Err(e) => panic!("Error connecting to Postgres server: {:#?}", e)
+    };
 }
